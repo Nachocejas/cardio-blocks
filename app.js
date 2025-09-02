6a7d155afe62e3522c6464bab68117f613b538ae
Nachocejas <ilopcej@gmail.com>
2025-09-02 12:23:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  initTheme();
-  cargarRutinas();
-  actualizarSelectRutinas();
-});
-
-document.addEventListener("DOMContentLoaded", () => {
   // Ocultar splash después de 1 segundo
   setTimeout(() => {
     document.getElementById("splash").classList.add("oculto");
   }, 1000);
-
-  // Lo que ya tenías
   initTheme();
   cargarRutinas();
   actualizarSelectRutinas();
 });
+
+
+
